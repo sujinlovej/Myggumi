c0cd97df07b522d358871ed1cd5ebb14d44b4f4b
KYDBM <KYDBM@DESKTOP-SHA9NV8>
2017-12-20 14:31:43 +0900

    First

---
+++ Myggumi/src/main/java/com/study/myggumi/MyggumiApplication.java
@@ -0,0 +1,12 @@
+package com.study.myggumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyggumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyggumiApplication.class, args);
+	}
+}
---
+++ Myggumi/src/main/java/com/study/myggumi/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.study.myggumi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/")
+	public ModelAndView index() {
+		
+		ModelAndView mv = new ModelAndView();
+		
+		mv.setViewName("index");
+		
+		return mv;
+		
+	}
+
+}
